f5e980d1dc73aa402162215e5a999bb83c096d60
Brushan <tommy.berggrens@gmail.com>
2020-07-22 14:06:48 +0200

    send and back buttons

--- client/components/Camera/Camera.jsx
+++ client/components/Camera/Camera.jsx
@@ -1,12 +1,13 @@
 
 import React, {useState, useEffect, useRef} from 'react';
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, ImageBackground} from 'react-native'
 import { Camera } from 'expo-camera'
 
 const CameraView = () => {
     const [hasPermission, setHasPermission] = useState(null);
     const [cameraRef, setCameraRef] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const [photo, setPhoto] = useState(null);
 
     useEffect (() => {
         (async () => {
@@ -23,7 +24,25 @@ const CameraView = () => {
     }
     return (
        <View style= {{ flex: 1, width: '100%'}}>
-           <Camera style={{flex: 1}} type={Camera.Constants.Type.back} ref={ref => {
+           {photo ? (<ImageBackground 
+            style={{ flex: 1}}
+            source={{ uri: photo.uri}}>
+            <TouchableOpacity
+                onPress={()=> {
+                    setPhoto(null);
+                }}>
+               <Text style={{color: 'white'}}>Back</Text> 
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={()=> {
+                    setPhoto(null);
+                }}>
+               <Text style={{color: 'white'}}>Send</Text> 
+            </TouchableOpacity>
+            </ImageBackground>
+            )
+           
+            : (<Camera style={{flex: 1}} type={Camera.Constants.Type.back} ref={ref => {
                setCameraRef(ref);
            }}>
                <View
@@ -33,8 +52,13 @@ const CameraView = () => {
                }}>
                 <TouchableOpacity style={{alignSelf: 'center'}} onPress={async()=>{
                     if (cameraRef) {
-                        let photo = await cameraRef.takePictureAsync();
+                        let photo = await cameraRef.takePictureAsync({
+                            quality: 0.1,
+                            base64: true,
+                            exif: true
+                        });
                         console.log('photo', photo);
+                        setPhoto(photo);
                     }
                 }}>
                 <View style={{
@@ -58,7 +82,7 @@ const CameraView = () => {
                 </View>
                 </TouchableOpacity>
                </View>
-           </Camera>
+           </Camera>)}
        </View>
     );
 }
